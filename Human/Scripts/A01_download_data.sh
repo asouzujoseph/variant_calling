8d803216112934668269e7785f64b17733c0da5e
Nnamdi Joseph Asouzu <61297245+asouzujoseph@users.noreply.github.com>
2022-08-17 23:44:48 -0400

    Create A01_download_data.sh

---
+++ Human/Scripts/A01_download_data.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd ..
+cd ..
+wget https://ngs-variants-training.s3.eu-central-1.amazonaws.com/ngs-variants-training.tar.gz
+tar -xvf ngs-variants-training.tar.gz
+rm ngs-variants-training.tar.gz
