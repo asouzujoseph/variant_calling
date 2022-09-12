5a5e6a7dec6a149db5802303516091683d2f73c0
Nnamdi Joseph Asouzu <61297245+asouzujoseph@users.noreply.github.com>
2022-09-11 23:03:45 -0400

    final

---
+++ mycobacterium/auto_read_qc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -e
+cd ../data/fastq/
+echo "Running FastQC ..."
+fastqc *.fastq
+
+mkdir -p ../../results/fastqc_untrimmed_reads
+echo "Saving FastQC results..."
+mv *.zip ../../results/fastqc_untrimmed_reads/
+mv *.html ../../results/fastqc_untrimmed_reads/
+
+cd ../../results/fastqc_untrimmed_reads/
+echo "Unzipping ..."
+for filename in *.zip
+do
+        unzip $filename
+done
+
+echo "saving summary..."
+cat */summary.txt > ../fastqc_summaries.txt
